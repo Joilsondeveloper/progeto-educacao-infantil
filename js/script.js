9a401ccccc9974ee01a60f7244dc7d3c7ece577f
Joilson <104251966+Joilsondeveloper@users.noreply.github.com>
2022-05-22 17:02:22 -0300

    script adicionado a pasta JS

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
+function btnAbrir() {
+   document.getElementsByClassName('menus-lateral')[0].style.width = '66%'
+   document.getElementsByClassName('menus-lateral')[0].style.marginRight = '4px'
+}
 
+function btnFechar() {
+   document.getElementsByClassName('menus-lateral')[0].style.width = '0'
+}
